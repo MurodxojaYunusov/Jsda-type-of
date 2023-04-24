a2a4d82ac191dd243b7fdb698175f209b888e8bf
MurodxojaYunusov <murodxojayunusov492@gmail.com>
2023-04-24 15:04:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = 'Murodxo"ja'; //String
+let age = 18; //number
+let isMarried = false; //Boolean 
+let firstName = undefined; // undefined
+let selectedColor = null; //null
+selectedColor = 'green';
+selectedColor = null;
+
+
+console.log(name)
